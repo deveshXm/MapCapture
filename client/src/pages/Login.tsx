29a53e0426a51541c4215b13a5d1b023bed4e884
Devesh Meena <nothefakedevesh@gmail.com>
2024-06-25 11:03:32 +0530

    fix: file name

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -9,7 +9,7 @@ import { setCredentials } from "../store/authSlice";
 import { Title } from "../components/ui/Title";
 import { GradientInput } from "../components/ui/Gradientinput";
 import GradientButton from "../components/ui/GradientButton";
-import { Label, LabelInputContainer } from "../components/ui/label";
+import { Label, LabelInputContainer } from "../components/ui/Label";
 import { AuroraBackground } from "../components/ui/aurora-background";
 
 const Login: React.FC = () => {
